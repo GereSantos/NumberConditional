21a9e4cce477c9bf678ab80bc94cc6b5d83b31f1
Geremias Santos <Geremias99999@gmail.com>
2022-06-13 13:38:09 -0300

    Exercise Number

---
+++ Number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Descobrir se o número é Impar ou par
+            int n = int.Parse(Console.ReadLine());
+
+            if(n % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
+        }
+    }
+}
